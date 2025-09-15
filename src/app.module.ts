866d2cadf1dc2acdf37e3f685cd96cfce857935b
Carlos Botina <carlos@Mac.lan>
2025-09-14 19:30:28 -0500

    [PMS-1]: Add Docker configuration and management scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { StatsModule } from './stats/stats.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
+import { HealthModule } from './health/health.module';
 import configValidation from './config/validation/config.validation';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
@@ -61,6 +62,7 @@ import { APP_GUARD } from '@nestjs/core';
     UsersModule,
     AuthModule,
     MailModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
